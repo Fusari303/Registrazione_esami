3b5ae6d1d3c0f3c3acd7226bfb139d458b1e8439
Fusari303 <cristianfusari303@gmail.com>
2022-04-04 22:00:57 +0200

    Verifica del funzionamento del progetto

---
+++ conf/config.php
@@ -0,0 +1,13 @@
+<?php
+
+const DB_HOST = 'localhost';
+const DB_NAME = 'registrazione_esami';
+const DB_USER = 'root';
+const DB_PASSWORD = '';
+const DB_CHAR = 'utf8';
+
+//Sottocartella dove viene eseguita l'applicazione
+const BASE_PATH = '/registrazione_esami';
+
+//Segreto del token JWT
+const JWT_SECRET = 'jwt_secret';
